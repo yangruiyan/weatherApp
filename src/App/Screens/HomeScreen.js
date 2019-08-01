f3eca3242607dd3340a09d834d8a12974838732a
apple <apple@appledeMacBook-Pro-3.local>
2019-08-01 15:12:46 +0800

    starting template

---
+++ src/App/Screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render () {
+        return (
+            <div>Hello Home!</div>
+        )
+    }
+}
+
+
+
+
+
+export default HomeScreen
---
+++ src/App/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import HomeScreen from './Screens/HomeScreen'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render () {
+        return (
+            <div>
+                <HomeScreen/>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default App
